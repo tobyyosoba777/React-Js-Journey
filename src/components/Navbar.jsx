7b0a4df6a2a4551797d9122ea555a907a9d11e4a
tobiloba <tobiosoba777@gmail.com>
2024-02-11 01:59:00 -0800

    Updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,4 @@
-import React from 'react'
-
+import React, { useState } from 'react'
 
 const Navbar = () => {
 
@@ -9,22 +8,42 @@ const Navbar = () => {
     }
 
     const displayTags = () => {
-        if(state.navTags.length === 0) return 'No Tags, add navbar Tags'
-        return state.navTags.map(tag => <li key={tag}>{tag}</li>)
+        if(state.navTags.length === 0) return 'No Tags, add navTags'
+        return state.navTags.map(tag => <li style={{
+            display: 'flex',
+            marginLeft: '20px',
+            justifyContent: 'space-between'
+        }} key={tag}>{tag}</li>)
     }
+
+    const [mode, setMode] = useState('Dark')
+
+    const toggleBackground = () => {
+        var classes = 'navbar navbar-expand-lg navbar-light bg-'
+        classes += mode === 'Dark'? 'light': 'dark'
+        return classes
+    }
+
+    const toggleMode = () => {
+        setMode(prevMode => (prevMode === 'Light' ? 'Dark' : 'Light'));
+      };    
+
   return (
-    <div>
+    <div className={toggleBackground()}>
       <div className="header">
+      <img style={{
+            height: 90,
+            borderRadius: '50%'
+        }} src={state.imageUrl} alt=""  />
+        <a href={state.imageUrl}>Towbbie</a>
         <p style={{
             display: 'flex',
             listStyleType: 'none',
             marginLeft: '20px'
         }} className='btn badge-primary'>{displayTags()}</p>
-        <img style={{
-            height: 90,
-            borderRadius: '50%'
-        }} src={state.imageUrl} alt=""  />
-        <a href={state.imageUrl}>Towbbie</a>
+        <button className='btn btn-sm' onClick={{toggleMode}}>{mode}</button>
+        <button className='btn btn-sm btn-primary' onClick={toggleMode}>{mode}</button>
+        
       </div>
     </div>
   )
